8190fd42da340f3a02d54cbe6f77975d501f4c99
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2024-03-03 21:55:29 -0500

    build: basic HTML elements

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import datetime
+from pyscript import document
 
+def my_function(event):
+    input_text = document.querySelector("#exp-due-date-input")
+    date = input_text.value
+
+    output_div = document.querySelector("#dates-output")
+    output_div.innerText = date.value
